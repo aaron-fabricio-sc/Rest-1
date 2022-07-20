1132fed4da65d436f124deafc8765b7c09d54237
aaron fabricio <aaronfabricio00@gmail.com>
2022-07-20 10:01:41 -0400

    segunda modificacion

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-const URL = "https://api.thedogapi.com/v1/images/search?limit=5";
+const URL =
+  "https://api.thedogapi.com/v1/images/search?limit=5&api_key=7f4a02b3-15b2-4937-8740-d4e74e548b9c";
 
 const peticion = async () => {
   const $imagenes = document.getElementById("imagenes");
+
+  const $containerFavoritos = document.getElementById("containerFavoritos");
   const getData = await fetch(URL);
   const json = await getData.json();
-  const fragment = document.createDocumentFragment();
+  const fragmentRandom = document.createDocumentFragment();
+  const fragmentFavoritos = document.createDocumentFragment();
+
   json.forEach((element) => {
     const $img = document.createElement("img");
+    const $botton = document.createElement("button");
     $img.src = element.url;
     $img.width = "350";
-    fragment.appendChild($img);
+    $botton.id = "clickFavoritos";
+    $botton.innerHTML = "Agregar a favoritos";
+    fragmentRandom.appendChild($img);
+    fragmentRandom.appendChild($botton);
     console.log($img);
   });
 
-  $imagenes.appendChild(fragment);
+  const $imgFavoritos = document.createElement("img");
+  const $article = document.createElement("article");
+  $templateFavorito = `
+        <img src="" alt="perro favorito">
+      <button class="btnEliminarFavoritos">Eliminar de favoritos</button>`;
+
+  $article.innerHTML = $templateFavorito;
+  fragmentFavoritos.appendChild($article);
+
+  $imagenes.appendChild(fragmentRandom);
+  $containerFavoritos.appendChild(fragmentFavoritos);
 };
